8cf5ea63786abc80141778ac1733e53cb81758e1
Labinot <krasniqilabinot42@gmail.com>
2022-11-08 23:37:07 +0100

    initial commit

---
+++ Labinot_Krasniqi-Portfolio/Labinot_Krasniqi-Portfolio/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Labinot_Krasniqi_Portfolio.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
