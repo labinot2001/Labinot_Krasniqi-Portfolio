bbc9e65475fd7b48e5fb569a4a0234185aac4f91
Labinot <krasniqilabinot42@gmail.com>
2022-11-12 22:04:41 +0100

    Shtimi i modelit Contact, disa rregullime ne front

---
+++ Labinot_Krasniqi-Portfolio/Labinot_Krasniqi-Portfolio/Models/Contact.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Labinot_Krasniqi_Portfolio.Models
+{
+    public class Contact
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "Full name")]
+        [Required(ErrorMessage = "Full name is required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "FullName length must be from 4 to 50 chars ")]
+        public string FullName { get; set; }
+
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "Email is required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Email length must be from 5 to 50 chars ")]
+        public string Email { get; set; }
+
+        [Display(Name = "Subject")]
+        [Required(ErrorMessage = "Subject is required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Subject length must be from 4 to 50 chars ")]
+        public string Subject { get; set; }
+
+        [Display(Name = "Message")]
+        [Required(ErrorMessage = "Message is required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Message length must be from 10 to 300 chars ")]
+        public string Message { get; set; }
+
+
+
+
+    }
+}
